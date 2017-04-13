5507a144584f74bbb29cc964f6d8bc70823110a1
Mathias <mathias.haegel@viacesi.fr>
2017-04-13 11:36:44 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+projetwebA2
+===========
+
+A Symfony project created on April 13, 2017, 11:17 am.
---
+++ src/ProjetWeb/SiteBundle/ProjetWebSiteBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ProjetWeb\SiteBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ProjetWebSiteBundle extends Bundle
+{
+}
